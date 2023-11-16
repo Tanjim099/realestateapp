af23bc0389107504b1d8321a553e90c781a615b4
Tanjim <tanjimseo@gmail.com>
2023-11-17 00:51:01 +0530

    Added user schema

---
+++ server/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "USER"
+    }
+}, { timestamps: true }
+);
+
+const users = mongoose.model("users", userSchema);
+export default users;
