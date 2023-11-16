d0e8da93ca4d7ede7930c9f30049635dcdd1ce97
Tanjim <tanjimseo@gmail.com>
2023-11-17 00:38:19 +0530

    mongodb database connection done

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+//configure env
+dotenv.config();
+
+//db config
+connectDB()
+
+
 const app = express();
 
-const PORT = 8000;
-app.listen(PORT, () => {
-    console.log("Server is running")
+app.get("/", (req, res) => {
+    res.send({
+        message: "Welcome to real Estate App"
+    })
 })
+
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
